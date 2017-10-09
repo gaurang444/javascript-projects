321abdd81994344099a37a0669c74c2339ec9183
Gaurang Bansal <gaurang444@users.noreply.github.com>
2017-10-09 18:29:39 +0530

    completed

--- quiz/man.js
+++ quiz/man.js
@@ -5,3 +5,39 @@ function check(){
 	var correct=0;
 
 
+
+	if(question1=="jaipur"){
+		correct++;
+	}
+	if(question2=="delhi"){
+		correct++;
+
+	}
+	if(question3=="washington"){
+		correct++;}
+	var messages=["Great job","Thats ok","You really need to do better"];
+	var pictures=["yepy.gif","sorry.gif","http://cdn.list25.com/wp-content/uploads/2011/11/penguifall.gif"];
+	var range;
+	if(correct<1){
+		range=2;
+	}
+	if(correct>0 && correct<3){
+		range=1;
+	}
+	if(correct>2){
+		range=0;
+	}
+
+
+
+
+
+
+document.getElementById("afert").style.visibility="visible";
+document.getElementById("message").innerHTML=messages[range];
+document.getElementById("afer").innerHTML="You got " + correct + " correct answers";
+document.getElementById("pic").src=pictures[range];
+
+
+
+}
